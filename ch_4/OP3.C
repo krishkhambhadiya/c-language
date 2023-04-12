fc0fddc12d6ae015f4fd56e83edd298373e7c9dd
krishkhambhadiya <khambhadiyakrish699@gmail.com>
2023-04-12 16:58:46 +0530

    c language

---
+++ ch_4/OP3.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,z;
+	printf("enter the value of x :");
+	scanf("%d",&x);
+	printf("\nenter the value of y :");
+	scanf("%d",&y);
+	z=(x+y)*(x+y)*(x+y);
+	printf("ans of %d",z);
+	getch;
+}
